70df56759028365912be1ea0b96c452a2558e980
atz3n <A_Aldemir@hotmail.de>
2019-12-16 14:02:25 +0100

    added build tests

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     "roots": [
-      "<rootDir>/test"
+        "<rootDir>/test"
     ],
     "transform": {
-      "^.+\\.tsx?$": "ts-jest"
+        "^.+\\.tsx?$": "ts-jest"
     }
-  }
+}
