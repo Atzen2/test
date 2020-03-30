8fa3df12365156690f3164e436b8e98b6cb275d8
atz3n <A_Aldemir@hotmail.de>
2020-03-30 09:28:52 +0200

    removed ardor-ts dependency

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,5 @@
-import { greetings, createPassphrase } from '../src/index'
+import { greetings } from '../src/index'
 
 test('greetings test', () => {
     expect(greetings('Anna')).toBeDefined();
-});
-
-test('createPassphrase test', () => {
-    expect(createPassphrase()).toBeDefined();
 });
